71f61c800fa0c277b36ef44a5e9c5c237858b4c8
alexandresanchograca <alexandresanchograca@gmail.com>
2024-05-16 17:43:51 +0100

    regular article grid done

--- sapo-web-clone/src/components/ArticleGrid/ArticleGrid.vue
+++ sapo-web-clone/src/components/ArticleGrid/ArticleGrid.vue
@@ -1,64 +1,67 @@
 <template>
   <div class="main-article-grid">
-    <div class="col first-col">
-      <MainNewsBox class="main-news-box"></MainNewsBox>
-      <NewsBox></NewsBox>
+    <img src="../../assets/news-files/big-img.jpeg" />
+    <div class="big-news-card">
+      <div class="red-pill"><p>EXCLUSIVO</p></div>
+
+      <h2>Anacom e Governo têm de incentivar consolidação</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, quidem
+        aspernatur repellendus accusantium voluptatibus sequi placeat ullam esse
+        ad minus tenetur.
+      </p>
     </div>
-    <div class="col first-col">
-      <MainNewsBox class="main-news-box"></MainNewsBox>
-      <NewsBox></NewsBox>
-    </div>
-    <div class="col first-col">
-      <NewsBox></NewsBox>
-      <NewsBox></NewsBox>
-    </div>
+    <NewsBox></NewsBox>
+    <NewsBox></NewsBox>
+    <NewsBox></NewsBox>
+    <NewsBox></NewsBox>
   </div>
 </template>
 
 <script>
-import MainNewsBox from "@/components/ArticleGrid/MainNewsBox.vue";
-import SmallNewsBox from "@/components/ArticleGrid/SmallNewsBox.vue";
 import NewsBox from "./NewsBox.vue";
-import LatestNews from "./LatestNews.vue";
-import Markets from "./Markets.vue";
 
 export default {
   components: {
-    MainNewsBox,
-    SmallNewsBox,
     NewsBox,
-    LatestNews,
-    Markets,
   },
 };
 </script>
 
 <style scoped>
 .main-article-grid {
-  margin: 15px;
-  margin-top: 40px;
-  display: flex;
-  align-items: start;
-  max-height: 650px;
-  gap: 20px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
 }
-.col > * {
-  background-color: white;
+.main-article-grid > * {
+  margin: 20px;
 }
-.first-col {
-  display: flex;
-  flex-direction: column;
+.main-article-grid img {
+  height: 350px;
+  grid-column: 1;
+  grid-row: 1;
 }
-.main-news-box {
-  margin: auto;
-  margin-bottom: 20px;
-  flex-grow: 1;
-  width: fit-content;
+.big-news-card h2 {
+  font-size: 1.3em;
+  font-weight: bold;
 }
-.latest-news-box {
-  width: 240px;
+.big-news-card p {
+  margin-top: 15px;
 }
-.markets-box {
-  width: 340px;
+
+.red-pill {
+  float: left;
+}
+.red-pill p {
+  float: left;
+  margin-top: 5px;
+  margin-right: 10px;
+  padding: 10px;
+  padding-top: 2px;
+  padding-bottom: 2px;
+  border-radius: 20px;
+  font-size: small;
+  color: white;
+  background-color: red;
 }
 </style>
