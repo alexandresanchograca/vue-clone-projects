68918b636723681a9d876ca307966400ad54fe24
alexandresanchograca <alexandresanchograca@gmail.com>
2024-05-29 10:37:22 +0100

    initial amazon website draft strucure

---
+++ amazon-vue-clone/src/App.vue
@@ -0,0 +1,32 @@
+<template></template>
+
+<script setup></script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap");
+
+:root {
+  background-color: rgb(244, 244, 244);
+}
+
+* {
+  margin: 0px;
+  font-family: "Open Sans", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+.home {
+  display: flex;
+  flex-direction: column;
+}
+
+.bold {
+  font-weight: bold;
+}
+
+.strong-bold {
+  font-weight: 800;
+}
+</style>
