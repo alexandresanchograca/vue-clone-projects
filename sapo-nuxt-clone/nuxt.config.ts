afbd20cc52266ad7795b1be486625d862c300787
alexandresanchograca <alexandresanchograca@gmail.com>
2024-06-30 12:47:41 +0100

    Defered font-awesome script to increase load performance.

--- sapo-nuxt-clone/nuxt.config.ts
+++ sapo-nuxt-clone/nuxt.config.ts
@@ -1,32 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: {
-    enabled: true,
-    timeline: {
-      enabled: true,
-    },
-  },
-  components: [
-    {
-      path: "~/components",
-      pathPrefix: false,
-    },
-  ],
-  app: {
-    head: {
-      title: "ECO - Economia Online",
-      script: [
-        {
-          src: "https://kit.fontawesome.com/3210827b43.js",
-          crossorigin: "anonymous",
+    devtools: {
+        enabled: true,
+        timeline: {
+            enabled: true,
         },
-      ],
-      link: [
-        {
-          rel: "stylesheet",
-          href: "https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap",
-        },
-      ],
     },
-  },
+    components: [
+        {
+            path: "~/components",
+            pathPrefix: false,
+        },
+    ],
+    app: {
+        head: {
+            title: "ECO - Economia Online",
+            script: [
+                {
+                    src: "https://kit.fontawesome.com/3210827b43.js",
+                    crossorigin: "anonymous",
+                    defer: true,
+                },
+            ],
+            link: [
+                {
+                    rel: "stylesheet",
+                    href: "https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap",
+                },
+            ],
+        },
+    },
 });
